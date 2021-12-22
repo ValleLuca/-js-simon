78463ffafdbdcf9b13f61518eeaa5976b65f3a69
ValleLuca <valle.lucaAb@outlook.it>
2021-12-22 17:25:03 +0100

    Aggiunte modifiche e js corretto

--- myscript.js
+++ myscript.js
@@ -1,27 +1,38 @@
-let memoria = document.getElementById("carte"); 
-let memoryArrRandom = [];
-let memoryArrUtente = [];
+let memoria = document.getElementById("carte");                 // Variabile per visualizzare le carte da ricordare
+let memoryArrRandom = [];                                       // Dichiarazione array Random
+let memoryArrUtente = [];                                       // Dichiarazione array Utente
+let quantiIndovinati = document.getElementById("risultato");    // Variabile per prendere il futuro contenitore per il risultato
 
 
-for(let i = 0; i < 5; i++){                                 // Generare 5 numeri random
+
+for(let i = 0; i < 5; i++){                                     // Generare 5 numeri random
     let dPc = Math.floor(Math.random() * 100);
-    memoryArrRandom.push(dPc);
+    memoryArrRandom.push(dPc);                                  // Array per aggiungere i random
 }
-console.log(memoryArrRandom);                               // controlo in console
-memoria.innerHTML = (memoryArrRandom);                      // stampa su html all'interno dello spam
+console.log(memoryArrRandom);                                   // Controlo in console
+memoria.innerHTML = (memoryArrRandom);                          // Stampa su html all'interno dello spam
 
-let memorizzazioneCarte = setInterval()
+setTimeout( function(){                                         // Attesa di 30 secondi prima di iniziare a calcolare e chiedere all'utente di inserire i dati
+    
+    let indovinati = [];                                        // Array per le carte indovinate
+    let sbagliati = [];                                         // Array per le carte indovinate
 
-for(let i = 0; i < 5; i++){                                  // Generare 5 numeri random
-    let imputUtente = parseInt(prompt("inserire i numeri")); //da metter dentro un ciclo
-    memoryArrUtente.push(imputUtente);
-}
+    for(let i = 0; i < 5; i++)
+    {                                                           
+        let imputUtente = parseInt(prompt("inserire i numeri")); // Chiedere all'utente di inserire 5 numeri
+        memoryArrUtente.push(imputUtente);
 
+            if(memoryArrUtente[i] == memoryArrRandom[i]){       // Se il numero digitato è corretto
+                indovinati.push(memoryArrUtente[i]);            // Se indovinati++                                                                  
+            }
+            else                                                // Else sbagliati++
+            {
+                sbagliati.push(memoryArrUtente[i]);
+            }
+        
+        
+    }
 
+    quantiIndovinati.innerHTML = ("Hai azzeccato " + indovinati.length + " carte e " + sbagliati.length + " non le hai azzecate.");  // Dire all'utente quanti ne ha indovinati e quanti sbagliati
 
-// aspettare 30 secondi
-// chiedere all'utente di inserire 5 numeri 
-// confrontare i risultait e far vedere quante caselle combaciano
-
-
-
+} , 30000 );
